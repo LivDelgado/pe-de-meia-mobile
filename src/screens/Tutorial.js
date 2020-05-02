ef07bbc775c8e73067dcefb89816cd71aea37dd5
Lívia Delgado <livia.delgado.ac@gmail.com>
2020-05-02 11:00:03 -0300

    Implementação da BottomTab

---
+++ src/screens/Tutorial.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from 'react-native-elements';
+import Constants from 'expo-constants';
+
+import Colors from '../constants/Colors';
+import logo from '../assets/images/logo.png';
+
+export default function Tutorial() {
+    const navigation = useNavigation();
+
+    return (
+        <View style = { styles.container }>
+            <Text style={{fontSize: 50, color: '#FFF'}}>TUTORIAL</Text>
+            <Image source={ logo } style={{ alignSelf: "center", justifyContent: "center" }} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      paddingTop: Constants.statusBarHeight,      
+      backgroundColor: Colors.pdmRoxo1,
+      justifyContent: "space-evenly"
+    }
+});
