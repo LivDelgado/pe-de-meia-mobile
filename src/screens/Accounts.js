21668b0ce2e1d016d5c5b144698167778bdbcb46
Aline Cristina <ninanerd15@gmail.com>
2020-05-02 19:53:11 -0300

    Adiciona input de saldo da conta

--- src/screens/Accounts.js
+++ src/screens/Accounts.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from 'react-native-elements';
 import Constants from 'expo-constants';
 
 import Colors from '../constants/Colors';
-import logo from '../assets/images/logo.png';
+import Card from '../components/Card';
 
 export default function Accounts() {
     const navigation = useNavigation();
 
     return (
         <View style = { styles.container }>
-            <Text style={{fontSize: 50, color: '#FFF'}}>CONTAS</Text>
-            <Image source={ logo } style={{ alignSelf: "center", justifyContent: "center" }} />
+
+            <Card>
+                <Text style={styles.titleText}>ADICIONE UMA NOVA CONTA E SEUS DADOS</Text>
+
+                <View style={styles.body}>
+
+                    <Text style={styles.bodyText}>SALDO DA CONTA: </Text>
+                    <View style={styles.currency}>
+                        <Text style={styles.currencyBodyText}>R$: </Text>
+                        <TextInput style={styles.inputStyle}/>
+                    </View>
+                    
+                </View>
+
+            </Card>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    titleText: {
+       fontSize: 15,
+       color: '#999999',
+    },
+    body: {        
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    },
+    currency:{
+        alignItems: "center",
+        marginTop: 16,
+        flexDirection: 'row', 
+    },
+    currencyBodyText: {
+        fontSize: 40,
+        color: '#5F1AB2',
+        fontWeight: 'bold',
+    },
+    bodyText: {
+        fontSize: 15,
+        color: '#5F1AB2',
+        textAlign: 'right',
+    },
+    inputStyle: {
+        minHeight: 50,
+        minWidth: '80%',
+        borderColor: '#5F1AB2',
+        borderWidth: 2,
+        borderRadius: 10,
+        fontFamily: "Roboto",
+        fontSize: 15,
+        color: '#5F1AB3'
+    },
     container: {
       flex: 1,
       alignItems: 'center',
